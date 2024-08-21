80bfba0d1eb9cd68d8caa8107553dd0045dab014
SHANIB C K <ckshanib8@gmail.com>
2024-08-21 18:51:54 +0530

    new design processing

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,25 +1,24 @@
-
 import React from "react";
 import { Link } from "react-router-dom";
-import "./HomePage.css";
 import HomeFiles from "./HomeFiles";
+import "./HomePage.css"; // Import the custom CSS file
 
 const HomePage = () => {
   return (
-    <div className="home-page">
+    <>
       <div className="hero-section">
-        <h1>Welcome to the IEDC</h1>
-        <p>Please find your certificate and download them</p>
+        <h1 className="hero-title">Welcome to the IEDC</h1>
+        <p className="hero-subtitle">Please find your certificate and download them</p>
       </div>
       <div className="info-section">
-        <h2>Certificates</h2>
+        <h2 className="info-title">Certificates</h2>
         <div className="feature-cards">
           <div className="feature-card">
             <HomeFiles />
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
